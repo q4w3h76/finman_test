4aaeb933ceec343407802d0113ae69ec142468d1
q4w3h76 <q4w3h76@internet.ru>
2024-10-25 11:44:22 +0500

    create service for transaction

---
+++ app/Services/TransactionService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Transaction;
+
+class TransactionService
+{
+    // public function getAllByFilter()
+
+    public function store(array $data): Transaction
+    {
+        $transaction = Transaction::create($data);
+        return $transaction;
+    }
+    
+    public function update(Transaction $transaction, array $data): void
+    {
+        $transaction->update($data);
+    }
+    
+    public function delete(Transaction $transaction): void
+    {
+        $transaction->delete();
+    }
+}
