02a5728a5920e9f6fc79adf2ee316c2fd25ecae9
q4w3h76 <q4w3h76@internet.ru>
2024-10-25 15:00:53 +0500

    add new routes for auth and register

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AuthController;
 
 Route::prefix('transactions')
+    ->middleware('auth')
     ->resource(
         'transactions', 
         TransactionController::class,
         ['except' => ['show']]
-);
+    );
+
+Route::controller(RegisterController::class)
+    ->prefix('register')
+    ->name('register.')
+    ->middleware('guest')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+    });
+
+Route::controller(AuthController::class)
+    ->name('auth.')
+    ->group(function () {
+        Route::prefix('login')
+            ->name('login.')
+            ->middleware('guest')
+            ->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::post('/', 'login')->name('login');
+            });
+        Route::middleware('auth')
+            ->post('/logout', 'logout')
+            ->name('logout');
+    });
