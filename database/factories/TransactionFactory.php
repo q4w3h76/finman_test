5d43c0f05025ae21b52c6e09a5a12d63b4c6fa3f
q4w3h76 <q4w3h76@internet.ru>
2024-10-25 09:54:32 +0500

    create transaction factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sum' => fake()->randomFloat(2),
+            'description' => fake()->sentence(),
+            'category' => fake()->randomElement(['income', 'consumption']),
+        ];
+    }
+}
