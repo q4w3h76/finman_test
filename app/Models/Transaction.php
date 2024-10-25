a77aa3b4f0758592e9725158d9b9b25f02e55179
q4w3h76 <q4w3h76@internet.ru>
2024-10-25 14:48:53 +0500

    add user method

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -13,5 +14,11 @@ class Transaction extends Model
         'sum',
         'description',
         'category',
+        'user_id'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
