f6407c69889d811e2cb688493adb9f7af333e428
q4w3h76 <q4w3h76@internet.ru>
2024-10-25 09:55:28 +0500

    create transaction index page

---
+++ resources/js/Pages/Transaction/Index.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <h1>{{title}}</h1>
+    <ul>
+        <li v-for="transaction in transactions" :key="transaction.id" class="">
+            <p>{{ transaction.description }}</p>
+            <p>{{ transaction.sum }}</p>
+            <p>{{ transaction.category }}</p>
+            <p>{{ transaction.created_at }}</p>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        transactions: Array
+    },
+}
+</script>
