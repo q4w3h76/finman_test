aaa54cf35307ac381fb2995b30dd049822c3a306
q4w3h76 <q4w3h76@internet.ru>
2024-10-26 11:49:22 +0500

    create layout

---
+++ resources/js/Components/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <Header/>
+    <div class="container mx-auto mt-20">
+        <slot/>
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
