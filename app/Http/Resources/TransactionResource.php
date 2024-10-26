93856a4229c1ce6f5c32147d9b7761680c2d804e
q4w3h76 <q4w3h76@internet.ru>
2024-10-26 12:34:45 +0500

    create resource and collection for transaction

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category' => $this->category,
+            'description' => $this->description,
+            'sum' => $this->sum,
+            'created_at' => $this->created_at->toDateString(),
+        ];
+    }
+}
